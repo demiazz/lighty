d481c9a3d2c1f8f26cf721791078f5825a599b4f
Alexey Plutalov <demiazz.py@gmail.com>
2016-10-01 02:31:53 +0300

    Generate banner with version, author and license information

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,13 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
 import buble from 'rollup-plugin-buble';
 import uglify from 'rollup-plugin-uglify';
+import saveLicense from 'uglify-save-license';
 
+
+const pkgPath = resolve(__dirname, '../package.json');
+const pkg = JSON.parse(readFileSync(pkgPath, { encoding: 'utf8'}));
 
 const config = {
   entry: 'src/index.js',
@@ -25,8 +32,29 @@ if (process.env.TARGET === 'umd') {
     : 'dist/lighty.umd.js';
   config.format = 'umd';
 
+  config.banner = [
+    '/*!',
+    ` * ${pkg.name} v${pkg.version}`,
+    ` * ${pkg.homepage}`,
+    ' *',
+    ` * Copyright ${pkg.author.name}`,
+    ` * Released under the ${pkg.license} license`,
+    ' */',
+  ].join('\n');
+
   if (process.env.NODE_ENV === 'production') {
-    config.plugins.push(uglify());
+    config.plugins.push(uglify({
+      output: {
+        comments: saveLicense,
+      },
+    }));
+
+    config.banner = `/*! ${[
+      `${pkg.name} v${pkg.version}`,
+      `${pkg.homepage}`,
+      `(c) ${pkg.author.name}`,
+      `${pkg.license} license`,
+    ].join(' | ')} */`;
   }
 }
 
