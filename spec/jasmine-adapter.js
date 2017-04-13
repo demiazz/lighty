4b059912a1efa4f2e5bdd7e75e66de2c8e64725a
Alexey Plutalov <demiazz.py@gmail.com>
2017-04-13 13:03:41 +0300

    Try fix bug with rewritable `document.readyState` in specs

--- spec/jasmine-adapter.js
+++ spec/jasmine-adapter.js
@@ -1,20 +1,30 @@
 /* eslint no-underscore-dangle: "off" */
 
 try {
+  const initialValue = document.readyState;
+
   Object.defineProperty(document, "readyState", {
-    value: document.readyState,
+    value: initialValue,
     writable: true
   });
 
-  document.addEventListener("readystatechange", () => {
-    if (document.readyState === "loading") {
-      document.readyState = "interactive";
-    } else if (document.readyState === "interactive") {
-      document.readyState = "complete";
-    }
-  });
+  document.readyState = "non-standard-value";
 
-  window.readyStateIsMockable = true;
+  if (document.readyState === "non-standard-value") {
+    document.readyState = initialValue;
+
+    document.addEventListener("readystatechange", () => {
+      if (document.readyState === "loading") {
+        document.readyState = "interactive";
+      } else if (document.readyState === "interactive") {
+        document.readyState = "complete";
+      }
+    });
+
+    window.readyStateIsMockable = true;
+  } else {
+    window.readyStateIsMockable = false;
+  }
 } catch (e) {
   window.readyStateIsMockable = false;
 }
