4ab80c2c7916548eb49d3d9c1e7ff04e6a07b4c9
Alexey Plutalov <demiazz.py@gmail.com>
2017-07-26 12:52:38 +0300

    Use `documentation.js` instead of `jsdoc-to-markdown` for documentation generation

--- build/docs.js
+++ build/docs.js
@@ -1,20 +1,25 @@
 const fs = require("fs");
 const path = require("path");
-const jsdoc2md = require("jsdoc-to-markdown");
+const documentation = require("documentation");
 
 const OPEN_TAG = "<!-- API: BEGIN -->";
 const CLOSE_TAG = "<!-- API: END -->";
 
-const readme = fs
-  .readFileSync(path.resolve(__dirname, "../README.md"))
-  .toString();
-const docs = jsdoc2md.renderSync({
-  files: path.resolve(__dirname, "../src/docs.js")
-});
-const intro = readme.slice(0, readme.indexOf(OPEN_TAG) + OPEN_TAG.length);
-const outro = readme.slice(readme.indexOf(CLOSE_TAG));
+function build(...files) {
+  return documentation.build(files, {});
+}
 
-fs.writeFileSync(
-  path.resolve(__dirname, "../README.md"),
-  `${intro}\n${docs}\n${outro}`
-);
+function format(docs) {
+  return documentation.formats.md(docs, {});
+}
+
+function write(docs) {
+  const readmePath = path.resolve(__dirname, "../README.md");
+  const readme = fs.readFileSync(readmePath);
+  const intro = readme.slice(0, readme.indexOf(OPEN_TAG) + OPEN_TAG.length);
+  const outro = readme.slice(readme.indexOf(CLOSE_TAG));
+
+  fs.writeFileSync(readmePath, `${intro}${docs}${outro}`);
+}
+
+build(path.resolve(__dirname, "../src/docs.js")).then(format).then(write);
